0171ee48859d4ca61c90014dcf4a50adffaa6df5
Nick Polishchuk <nickun@gmail.com>
2019-02-13 19:00:31 +0100

    Added Nick's test

--- BuildTreeQuestConsole/Program.cs
+++ BuildTreeQuestConsole/Program.cs
@@ -13,12 +13,12 @@ namespace BuildTreeQuestConsole
     [ClrJob]
     public class Program
     {
-        [Benchmark(Baseline = true)]
-        public void Test_SimpleSlowTreeBuilder()
-        {
-            IList<ProjectLine> testData = TestDataLoader.LoadDemoData(TestDataLoader.DemoGenDataFileName);
-            IList<ProjectLine> resTestData = SimpleSlowTreeBuilder.BuildTree(testData);
-        }
+        //[Benchmark(Baseline = true)]
+        //public void Test_SimpleSlowTreeBuilder()
+        //{
+        //    IList<ProjectLine> testData = TestDataLoader.LoadDemoData(TestDataLoader.DemoGenDataFileName);
+        //    IList<ProjectLine> resTestData = SimpleSlowTreeBuilder.BuildTree(testData);
+        //}
 
         // put your test here
         [Benchmark]
@@ -26,23 +26,34 @@ namespace BuildTreeQuestConsole
         {
             IList<ProjectLine> testData = TestDataLoader.LoadDemoData(TestDataLoader.DemoGenDataFileName);
             IList<ProjectLine> resTestData = TreeBuilderTM.BuildTree(testData);
+		}
+		
+		[Benchmark]
+        public void Test_TreeBuilderNick()
+        {
+            IList<ProjectLine> testData = TestDataLoader.LoadDemoData(TestDataLoader.DemoGenDataFileName);
+            IList<ProjectLine> resTestData = TreeBuilderNick.BuildTree(testData);
         }
 
         static void Main(string[] args)
         {
-            // load source test data
-            const int genChaptersCount = 500;
-            var rnd = new Random(DateTime.Now.Millisecond);
+            // generate source test data
+            //const int genChaptersCount = 500;
+            //var rnd = new Random(DateTime.Now.Millisecond);
 
-            IList<ProjectLine> testData = TestDataLoader.GenerateDemoData(genChaptersCount);
-            File.WriteAllLines(TestDataLoader.DemoGenDataFileName, testData.Select(i => i.Chapter).OrderBy(l => rnd.Next(genChaptersCount)));
+            //IList<ProjectLine> testData = TestDataLoader.GenerateDemoData(genChaptersCount);
+            //File.WriteAllLines(TestDataLoader.DemoGenDataFileName, testData.Select(i => i.Chapter).OrderBy(l => rnd.Next(genChaptersCount)));
 
             // simple slow build test
+            IList<ProjectLine> testData = TestDataLoader.LoadDemoData(TestDataLoader.DemoGenDataFileName);
             RunTest(testData, SimpleSlowTreeBuilder.BuildTree);
 
             // put your test here
             testData = TestDataLoader.LoadDemoData(TestDataLoader.DemoGenDataFileName); // load the same data again
             RunTest(testData, TreeBuilderTM.BuildTree);
+
+            testData = TestDataLoader.LoadDemoData(TestDataLoader.DemoGenDataFileName);
+            RunTest(testData, TreeBuilderNick.BuildTree);
 
             var summary = BenchmarkRunner.Run<Program>();
 
