57b09af81d2efb0e24ca99213fb9679356770246
Nick Polishchuk <nickun@gmail.com>
2019-02-15 08:19:14 +0100

    Added some methods

---
+++ BuildTreeQuestConsole/TreeBuilderAlexV.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BuildTreeQuestConsole
+{
+    public class TreeBuilderAlexV
+    {
+        public static IList<ProjectLine> BuildTree(IList<ProjectLine> testData)
+        {
+            var lookup = testData.ToDictionary(td => td.Chapter, td => td);
+            return testData.Select(pl =>
+                {
+                    int p = pl.Chapter.LastIndexOf('.');
+                    if (p >= 0)
+                    {
+                        string parentChapter = pl.Chapter.Substring(0, p);
+                        if (lookup.ContainsKey(parentChapter))
+                        {
+                            var parentpl = lookup[parentChapter];
+                            pl.ParentId = parentpl.Id;
+                            parentpl.HasChildren = true;
+                        }
+                    }
+                    return pl;
+                })
+                .ToList();
+        }
+    }
+}
--- BuildTreeQuestConsole/TreeBuilderTM.cs
+++ BuildTreeQuestConsole/TreeBuilderTM.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace BuildTreeQuestConsole
 {
@@ -14,7 +16,7 @@ namespace BuildTreeQuestConsole
             return chapter.Substring(0, dot);
         }
 
-        public static IList<ProjectLine> BuildTree(IList<ProjectLine> testData)
+        public static IList<ProjectLine> BuildTree1(IList<ProjectLine> testData)
         {
             var nodeCollector = testData.ToDictionary(i => i.Chapter);
             var chapters = testData.OrderBy(pl =>
@@ -35,6 +37,49 @@ namespace BuildTreeQuestConsole
                 c.ParentId = val.Id;
                 return c;
             }).Where(c => c != null).ToList();
+        }
+
+        public static IList<ProjectLine> BuildTree(IList<ProjectLine> testData)
+        {
+            var nodeCollector = testData.ToDictionary(i => i.Chapter);
+            int count = testData.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                var c = testData[i];
+                int dot = c.Chapter.LastIndexOf('.');
+                if (dot < 0) continue;
+                if (nodeCollector.TryGetValue(c.Chapter.Substring(0, dot), out var val))
+                {
+                    val.HasChildren = true;
+                    c.ParentId = val.Id;
+                }
+            }
+
+            return testData;
+        }
+
+        public static IList<ProjectLine> BuildTreeParallel(IList<ProjectLine> testData)
+        {
+            var nodeCollector = testData.ToDictionary(i => i.Chapter);
+
+            var rangePartitioner = Partitioner.Create(0, testData.Count);
+            Parallel.ForEach(rangePartitioner, (range, loopState) =>
+            {
+                for (int i = range.Item1; i < range.Item2; i++)
+                {
+                    var c = testData[i];
+                    int dot = c.Chapter.LastIndexOf('.');
+                    if (dot < 0) continue;
+                    if (nodeCollector.TryGetValue(c.Chapter.Substring(0, dot), out var val))
+                    {
+                        val.HasChildren = true;
+                        c.ParentId = val.Id;
+                    }
+                }
+            });
+
+            return testData;
         }
     }
 }
